666e3dd193f84703c10e48dde1816ee5b31b159a
友人Ｓ <YuuJinS@users.noreply.github.com>
2022-05-21 03:40:17 +0900

    Build Empty Bot

--- GuildAssistant/Program.cs
+++ GuildAssistant/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(Properties.Settings.Default.TOKEN);
+﻿using Discord;
+using Discord.WebSocket;
+using System.Configuration;
+
+public class Program
+{
+    private DiscordSocketClient _client;
+
+    public static Task main(string args[]) => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        var token = ConfigurationManager.AppSettings.Get("TOKEN");
+
+        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+        // var token = File.ReadAllText("token.txt");
+        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+        
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
