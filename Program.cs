43321d78f7dc9b87f1c124da24af222f762bb07a
友人Ｓ <YuuJinS@users.noreply.github.com>
2022-05-29 14:27:23 +0900

    Setup Interaction Handler

--- Program.cs
+++ Program.cs
@@ -1,33 +1,47 @@
 ﻿public class Program
 {
-    public static Task Main(string[] args) => new Program().MainAsync();
-
-    private DiscordSocketClient? _client;
-
+    public static Task Main() => new Program().MainAsync();
     public async Task MainAsync()
     {
-        _client = new DiscordSocketClient();
 
-        _client.Log += Log;
 
-        //  You can assign your bot token to a string, and pass that in to connect.
-        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        using IHost host = Host.CreateDefaultBuilder()
+        .ConfigureServices((_, services) =>
+         services
+         .AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig
+         {
+             GatewayIntents = GatewayIntents.AllUnprivileged,
+             AlwaysDownloadUsers = true
+         }
+         ))
+         .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+         .AddSingleton<GuildAssistant.InteractionHandler>()
+         .AddSingleton(x => new CommandService())
+         )
+         .Build();
+        await RunAsync(host);
+    }
+    public async Task RunAsync(IHost host)
+    {
+        using IServiceScope serviceScope = host.Services.CreateScope();
+        IServiceProvider provider = serviceScope.ServiceProvider;
+
+        var _client = provider.GetRequiredService<DiscordSocketClient>();
+        var slashCommands = provider.GetRequiredService<InteractionService>();
+        await provider.GetRequiredService<GuildAssistant.InteractionHandler>().InitializeAsync();
+
+        _client.Log += async (LogMessage messages) => Console.WriteLine(messages.Message);
+        slashCommands.Log += async (LogMessage messages) => Console.WriteLine(messages.Message);
+
+        _client.Ready += async () =>
+        {
+            Console.WriteLine($"Login as{_client.CurrentUser.Username.ToString()}");
+            await slashCommands.RegisterCommandsGloballyAsync(true);
+        };
         var token = ConfigurationManager.AppSettings.Get("TOKEN");
-
-        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-        // var token = File.ReadAllText("token.txt");
-        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
-
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
 
-        // Block this task until the program is closed.
         await Task.Delay(-1);
-    }
-    private Task Log(LogMessage msg)
-    {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
     }
 }
